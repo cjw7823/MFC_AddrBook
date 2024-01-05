c0ea74d4fea213265ebf8718e28b44f54171da32
JaewonChoi <cjw7823@elviss.co.kr>
2024-01-05 19:22:17 +0900

    초기 커밋.

---
+++ AddrBook/CMyFormView.cpp
@@ -0,0 +1,65 @@
+﻿// CMyFormView.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "AddrBook.h"
+#include "CMyFormView.h"
+
+
+// CMyFormView
+
+IMPLEMENT_DYNCREATE(CMyFormView, CFormView)
+
+CMyFormView::CMyFormView()
+	: CFormView(IDD_FORMVIEW)
+{
+
+}
+
+CMyFormView::~CMyFormView()
+{
+}
+
+void CMyFormView::OnDraw(CDC* pDC)
+{
+	CListBox* pListBox = static_cast<CListBox*>(GetDlgItem(IDC_LIST1));
+	if (pListBox != nullptr)
+	{
+		listbox = pListBox;
+	}
+}
+
+void CMyFormView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMyFormView, CFormView)
+	ON_BN_CLICKED(IDC_BUTTON2, &CMyFormView::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// CMyFormView 진단
+
+#ifdef _DEBUG
+void CMyFormView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void CMyFormView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// CMyFormView 메시지 처리기
+
+
+void CMyFormView::OnBnClickedButton2()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
---
+++ AddrBook/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// AddrBook.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDD_FORMVIEW                    101
+#define IDR_MAINFRAME                   128
+#define IDR_AddrBookTYPE                130
+#define IDD_DIALOG1                     313
+#define IDD_DIALOG_NEW_ADDR             313
+#define IDCANCEL                        1000
+#define IDC_EDIT_NAME                   1001
+#define IDC_EDIT_PHONE                  1003
+#define IDC_BUTTON2                     1020
+#define IDC_BUTTON3                     1022
+#define IDC_LIST2                       1024
+#define IDC_LIST1                       1024
+#define ID_32771                        32771
+#define ID_New_Addr                     32772
+#define ID_32773                        32773
+#define ID_S                            32774
+#define ID_SEARCH                       32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        318
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1025
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
